0d51bcf97beac5265a71d649858ebcba8fd5757d
Wallace <wallace.12.marinho@gmail.com>
2026-02-15 11:58:10 -0500

    Primeira classe introdução ao curso

---
+++ src/academy/devdojo/maratonajava/logicadeprogramacao/OlaMundo.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.logicadeprogramacao;
+
+public class OlaMundo
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Olá mundo");
+    }
+}
