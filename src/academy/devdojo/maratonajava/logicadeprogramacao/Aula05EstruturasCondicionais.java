4c11057c50835745d41fdc73f71252d1f3a44bc8
Wallace <wallace.12.marinho@gmail.com>
2026-02-17 14:49:19 -0500

    Aula 05 e 06 concluidas

--- src/academy/devdojo/maratonajava/logicadeprogramacao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/logicadeprogramacao/Aula05EstruturasCondicionais.java
@@ -4,6 +4,7 @@ public class Aula05EstruturasCondicionais
 {
     public static void main(String[] args)
     {
+        //If, else if e else
         Double salarioAnual = 100000.0;
         Double valorImposto, salarioLiquido;
         String relatorio;
@@ -27,5 +28,34 @@ public class Aula05EstruturasCondicionais
             relatorio = "Você vai pagar 49.50% de imposto, o total de imposto cobrado foi de " + valorImposto + ", e seu salario liquido é de " + salarioAnual;
         }
         System.out.println(relatorio);
+
+
+        //Switch
+        byte dia = 5; // char, int, byte, short, enum e String
+
+        switch (dia)
+        {
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("segunda");
+                break;
+            case 3:
+                System.out.println("terca");
+                break;
+            case 4:
+                System.out.println("quarta");
+                break;
+            case 5:
+                System.out.println("quinta");
+                break;
+            case 6:
+                System.out.println("sexta");
+                break;
+            case 7:
+                System.out.println("sabado");
+                break;
+        }
     }
 }
