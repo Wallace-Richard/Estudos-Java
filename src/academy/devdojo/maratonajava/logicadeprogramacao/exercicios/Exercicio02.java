e3ccfa656abe84826f6010589f6702acbc105d1c
Wallace <wallace.12.marinho@gmail.com>
2026-02-17 17:09:08 -0500

    Exercicios sobre For e While

---
+++ src/academy/devdojo/maratonajava/logicadeprogramacao/exercicios/Exercicio02.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.logicadeprogramacao.exercicios;
+
+import javax.imageio.plugins.tiff.TIFFImageReadParam;
+
+public class Exercicio02
+{
+    public static void main(String[] args)
+    {
+        /*
+        Localizador de Alvo
+        Objetivo: Interromper o processamento assim que encontrar o que precisa.
+        O Desafio: Imagine que você tem uma contagem de 1 a 100.
+        Regra: O laço deve rodar, mas assim que atingir o número 7, deve imprimir "Alvo encontrado!" e usar o break para não contar até 100.
+        */
+
+        for (int i = 1; i <= 100; i++)
+        {
+            int numeroEsperado = 7;
+
+            if (i == numeroEsperado)
+            {
+                System.out.println("Numero encontrado: " + i);
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/logicadeprogramacao/exercicios/Exercicio03.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.logicadeprogramacao.exercicios;
+
+public class Exercicio03
+{
+    public static void main(String[] args)
+    {
+        /*
+        Cronômetro Regressivo
+        Objetivo: Praticar a condição de parada do while.
+        Desafio: Crie uma variável int contador = 10;.
+        Regra: Enquanto o contador for maior que 0, imprima o valor e diminua 1 (contador--). Quando chegar a 0, imprima "Fogo!".
+        */
+        int count = 11;
+
+        System.out.println("Tempo até o incendio: ");
+        while (count >= 0)
+        {
+            count--;
+            System.out.println(count + " segundos");
+            if (count == 0)
+            {
+                System.out.println("Fogooooooo!!!!!!");
+                break;
+            }
+        }
+    }
+}
